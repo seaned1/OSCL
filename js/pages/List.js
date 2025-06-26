8827d78ba86637b22a770732820b4d716d9d5d45
Blendro <matthew.yip1418@gmail.com>
2025-06-25 23:23:46 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -50,10 +50,9 @@ export default {
                             <div class="type-title-sm">ID</div>
                             <p>{{ level.id }}</p>
                         </li>
-                        <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
-                        </li>
+
+
+
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
